4ca44ca873f0f38c76666b613784f1f9f2c10b5f
han8909227 <hbao2016@hotmail.com>
2017-07-12 17:10:34 -0700

    finalized version 1! Works like a charm!

--- app.js
+++ app.js
@@ -1,119 +1,112 @@
-//
-// var askOne = prompt("Do you consider me as true developer? yes or no").toUpperCase()
-//
-// if (askOne === "YES"){
-//   alert("Great! Let's test if you're right! ");
-//   console.log("a potential believer just arrived!");
-//
-// } else if (askOne === "NO") {
-//   alert("really? Let's prove you wrong!");
-//   console.log("warning! he could be an alien !");
-//
-// } else{
-//   alert("I need a yes or no please");
-// }
-//
-//
-//
-// var askTwo = prompt("Do you find my code clean and organized in anyway? yes or no ").toUpperCase()
-//
-// if (askTwo === "YES"){
-//   alert("You bet they are! ");
-//   console.log("The answer to question 2 is " + askTwo + ", organization skill proven!");
-//
-// } else if (askTwo === "NO") {
-//   alert("Thank you for the advice!");
-//   console.log("The answer to question 2 is " + askTwo + ", he must've seem wrose codes before");
-//
-// } else{
-//   alert("I need a yes or no please");
-// }
-//
-//
-// var askThree = prompt("Do you think I've created simple html text and have effectively used css & JS").toUpperCase()
-//
-// if (askThree === "YES"){
-//   alert("Thanks! ");
-//   console.log("The answer to question 2 is " + askThree + ", coding skill proven!");
-//
-// } else if (askThree === "NO") {
-//   alert("I see, let me improve on that");
-//   console.log("The answer to question 3 is " + askThree + ", I must improve in order to impress!");
-//
-// } else{
-//   alert("I need a yes or no please");
-// }
-//
-//
-// var askFour = prompt("Are my a bug lover?").toUpperCase()
-// switch(askFour){
-// case 'YES':
-//   alert("No! not in both real and virtual world! ");
-//   console.log("The answer to question 2 is " + askFour + ", he must love bugs...");
-//   break;
-//
-// case 'NO':
-//   alert("Yep, I am a debugger!");
-//   console.log("The answer to question 4 is " + askFour + ", he got it right!");
-//   break;
-//
-// default:
-//   alert("I need a yes or no please");
-//   break; }
-//
-//
-// var askFive = prompt("Do you think I am ready to start my career as a software developer?").toUpperCase()
-// switch(askFive){
-// case 'YES':
-//   alert("Thank you! Please feel free to reach out for any career opportunity!");
-//   console.log("The answer to question 2 is " + askFour + ", omg I'm getting hired");
-//   break;
-//
-// case 'NO':
-//   alert("Thanks, please leave a comment on how I can improve");
-//   console.log("The answer to question 4 is " + askFour + ", he got it right!");
-//   break;
-//
-// default:
-//   alert("I need a yes or no please");
-//   break; }
-//
-// var comment = prompt("Enter comment here: ")
-// console.log("The user's commend is: " + comment )
-//
-//
-// var userResponse = [];
-// var chanceLeft = 4;
-//
-// var askSixTxt = 'Guess again, 1-20. ';
-//
-//
-// var askSix = "Guess a number from 1-20";
-// var askSixT2 = askSixTxt + ' You got ' + chanceLeft + ' chances left!';
-// var askSixT3 = askSixTxt + ' You got '+ chanceLeft + ' chances left!';
-// var askSixT4 = askSixTxt + ' You got ' + chanceLeft + ' chance left!';
-//
-//
-//
+
+var totalRight = 0;
+
+
+//question 1
+var askOne = prompt("Do you think I like dogs?").toUpperCase()
+if (askOne === "YES" || "Y"){
+  alert("I love dogzzzz !");
+  console.log("dog lovers");
+  totalRight += 1;
+
+} else if (askOne === "NO" || "N") {
+  alert("really? Who doesn't like dogzzz!");
+  console.log("warning! he could be an alien !");
+
+} else{
+  alert("I need a yes or no please");
+}
+
+
+//question 2
+var askTwo = prompt("Do you find my code clean and organized in anyway? yes or no ").toUpperCase()
+if (askTwo === "YES" || "Y"){
+  alert("You bet they are! ");
+  console.log("The answer to question 2 is " + askTwo );
+  totalRight += 1;
+
+} else if (askTwo === "NO" || "N") {
+  alert("You must have seen worse !!");
+  console.log("The answer to question 2 is " + askTwo );
+
+} else{
+  alert("I need a yes or no please");
+}
+
+
+//question 3
+var askThree = prompt("Do you think I can swim?").toUpperCase()
+if (askThree === "YES" || "Y"){
+  alert("I drink like a fish when I go swim... ");
+  console.log("The answer to question 2 is " + askThree );
+  totalRight += 1;
+
+} else if (askThree === "NO" || "N") {
+  alert("That is correct");
+  console.log("The answer to question 3 is " + askThree );
+
+} else{
+  alert("I need a yes or no please");
+}
+
+
+//question 4
+var askFour = prompt("Are my a bug lover?").toUpperCase()
+if(askFour === 'YES' || 'Y'){
+  alert("No! not in both real and virtual world! ");
+  console.log("The answer to question 2 is " + askFour );
+  totalRight += 1;
+
+} else if (askFour === 'NO' || 'N'){
+  alert("Yep, I am a debugger!");
+  console.log("The answer to question 4 is " + askFour );
+
+} else{
+  alert("I need a yes or no please");
+ }
+
+
+
+//question 5
+var askFive = prompt("Do you think I have traveled to China before?").toUpperCase()
+if(askFive === 'YES' || 'Y'){
+  alert("Of course I was born there ");
+  console.log("The answer to question 2 is " + askFour );
+  totalRight += 1;
+
+} else if (askFive === 'NO' || 'N'){
+  alert("I have tried Panda Express in China");
+  console.log("The answer to question 4 is " + askFour );
+
+} else {
+  alert("I need a yes or no please"); }
+
+
+
+//question 6
 
 var myNumber = 8;
-var prediction = prompt("I'm thinking of a number between 1 and 20. What is it?");
 var exitVariable = true;
 var userArray = [];
-var trailLeft = 4 - userArray.length;
+var trailLeft = 4;
 
 while (userArray.length < 4 && exitVariable) {
+  var prediction = parseInt(prompt("I'm thinking of a number between 1 and 20. What is it?"));
     if(prediction === myNumber){
         alert("That's Correct!" + myNumber + " is my luck number!!");
         exitVariable = false;
-    }else if (prediction > myNumber) {
+        totalRight += 1;
+
+    } else if(prediction > myNumber) {
         userArray.push(prediction);
-        prediction = prompt("I was not thinking " + prediction + ". It's too high. Guess Again");
+        trailLeft -=1;
+        alert("Too high!");
         alert("You got " + trailLeft + " chances left!");
 
     } else{
       userArray.push(prediction);
-      prediction = prompt("I was not thinking " + prediction + ". It's too low. Guess again");
+      trailLeft -=1;
+      alert("Too low!");
       alert("You got " + trailLeft + " chances left!");
 
     }
@@ -121,52 +114,43 @@ while (userArray.length < 4 && exitVariable) {
 
 if (exitVariable){
   alert("Too bad you ran out of chances, my lucky number is 8 !");
-
-
+  console.log(userArray);
 }
 
 
 
-//
-// while(userResponse.length < 5){
-//   prompt(askSix);
-//   if (askSix >= 10){
-//     alert('Your number is too high!');
-//     chanceLeft -- ;
-//     userResponse.push(askSix);
-//
-//     prompt(askSixT2);
-//     if (askSixT2 >= 10){
-//       alert('Too high again!');
-//       chanceLeft --;
-//       userResponse.push(askSixT2);
-//
-//       prompt(askSixT3);
-//       if(askSixT3 >= 10){
-//         alert('Too high man!')
-//         chanceLeft --;
-//         userResponse.push(askSixT3);
-//
-//         prompt(askSixT4);
-//         if(askSixT4 >= 10){
-//           alert('You used up all chances! Goodbye!')
-//           chanceLeft --;
-//           userResponse.push(askSixT4);
-//
-//         }
-//       }
-//     }
-//   } else if(askSix < 9){
-//     alert('Your number is too low');
-//
-//
-//   } else{
-//     alert('You got it right!');
-//     alert('9 is my lucky number');
-//     numAttempt = 5;
-//
-//   }
-//
-// console.log(userResponse);
-//
-// }
+//question 7
+
+var mySport = ['table tennis','basketball'];
+var userSport = [];
+var exitSport = true;
+var chanceLeft = 6;
+
+while(userSport.length < 6 && exitSport){
+  var predictSport = prompt("What do you think my favorite sport is ?").toLowerCase();
+  if(predictSport === mySport[0] || predictSport === mySport[1]){
+    alert("That is correct! " + predictSport + " is one of my favorite sport");
+    exitSport = false;
+    totalRight += 1;
+
+  } else{
+    userSport.push(predictSport);
+    chanceLeft -=1;
+    alert("Not exactly! You got " + chanceLeft + " chances left!");
+  }
+
+}
+
+if (exitSport){
+  alert("You ran out of chances. My favorite sports are table tennis and basketball!");
+}
+
+
+// number of right response
+if (totalRight >= 5){
+  alert("Congradulation! You got " + totalRight + "/7 questions right!!")
+
+} else{
+  alert("Oh no! You only got " + totalRight + "/7 questions right")
+
+}
